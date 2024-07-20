d90fce19af92d59ae5ee02ca0691b42ed1b5040c
Marcos Venicio <marcos.venicio1230@hotmail.com>
2024-07-20 09:40:29 -0300

    feat: First Commit

---
+++ JSSalgadosDoces/src/main/java/com/JSSalgadosEDoces/JSSalgadosDoces/JsSalgadosDocesApplication.java
@@ -0,0 +1,13 @@
+package com.JSSalgadosEDoces.JSSalgadosDoces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsSalgadosDocesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsSalgadosDocesApplication.class, args);
+	}
+
+}
---
+++ JSSalgadosDoces/src/test/java/com/JSSalgadosEDoces/JSSalgadosDoces/JsSalgadosDocesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JSSalgadosEDoces.JSSalgadosDoces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsSalgadosDocesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
