6d6ad8088e5e6d3cf4b56db9d7c12506c0a140cc
Marcos Venicio <marcos.venicio1230@hotmail.com>
2024-07-24 20:36:41 -0300

    fix: Removed dependencys

--- JSSalgadosDoces/src/main/java/com/JSSalgadosEDoces/JSSalgadosDoces/Controller/ProductController.java
+++ JSSalgadosDoces/src/main/java/com/JSSalgadosEDoces/JSSalgadosDoces/Controller/ProductController.java
@@ -1,20 +1,12 @@
 package com.JSSalgadosEDoces.JSSalgadosDoces.Controller;
 
-import com.JSSalgadosEDoces.JSSalgadosDoces.HandleException.ApiException;
-import com.JSSalgadosEDoces.JSSalgadosDoces.Models.DTOs.ProductDTO;
 import com.JSSalgadosEDoces.JSSalgadosDoces.Models.Product;
 import com.JSSalgadosEDoces.JSSalgadosDoces.Services.ProductServices;
-import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.math.BigDecimal;
 import java.net.URI;
 import java.util.List;
 
